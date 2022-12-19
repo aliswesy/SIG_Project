3e059aae70647dcac8d68c2853efbea9b7e81c1e
AliSwesy <aliswesy@outlook.com>
2022-12-19 14:08:57 +0200

    [ADD] Add design UI for the project

---
+++ src/main/java/SIG.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+/**
+ *
+ * @author needf
+ */
+public class SIG {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
